919ee484428367cff90fa9f3e19a11cb720b7ea9
michele ercolino <michele.ercolino45@gmail.com>
2017-07-03 11:46:36 +0100

    update database add street address 2

---
+++ ContactWeb/Migrations/201707031041563_addStreetAddress2.cs
@@ -0,0 +1,16 @@
+namespace ContactWeb.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addStreetAddress2 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
